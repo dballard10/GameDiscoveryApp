e4b4a21171e005dff744a207ae2e18f14c719587
dballard10 <dylanballard55@gmail.com>
2025-01-20 14:24:13 -0500

    Created GameCard Container for gamecards and their skeletons.

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,9 +3,9 @@ import { SkeletonText } from "@/components/ui/skeleton";
 
 const GameCardSkeleton = () => {
   return (
-    <Card.Root width="300px" borderRadius={10} overflow="hidden">
+    <Card.Root>
       <Skeleton height="200px" />
-      <Card.Body>
+      <Card.Body height="100px">
         <SkeletonText />
       </Card.Body>
     </Card.Root>
