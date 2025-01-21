16d2c32b69bce63fb71385a6d913b2d6f8bb21a9
dballard10 <dylanballard55@gmail.com>
2025-01-20 20:33:56 -0500

    Ship genres with app.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import { Game } from "@/components/hooks/useGames";
 import getCroppedImageUrl from "@/services/img-url";
-import { Card, Image, HStack } from "@chakra-ui/react";
+import { Card, Image, HStack, Heading } from "@chakra-ui/react";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 
@@ -12,14 +12,14 @@ const GameCard = ({ game }: Props) => {
   return (
     <Card.Root>
       <Image src={getCroppedImageUrl(game.background_image)} />
-      <Card.Header fontSize="2xl">{game.name}</Card.Header>
       <Card.Body>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </Card.Body>
     </Card.Root>
   );
